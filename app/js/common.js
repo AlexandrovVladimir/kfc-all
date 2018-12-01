3b3647037286941295b1aaacc78842cd5ad311dc
alz <alexey.zhukov@ticos-systems.ch>
2018-12-01 23:47:55 +0200

    add

--- app/js/common.js
+++ app/js/common.js
@@ -1,2 +1,7 @@
 $(document).ready(function () {
+  /*mentors*/
+  $('.mentors-list__image').on('click', function() {
+    $('.mentors-data__name').html($(this).next().html());
+    $('.mentors-data__description').html($(this).next().next().html());
+  });
 });
